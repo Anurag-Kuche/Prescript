ca8045b362d0e907f1f54021407d5a7c51b9e390
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-17 22:28:13 +0530

    Doctors added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import SpecialityMenu from "../components/SpecialityMenu"
+import TopDoctors from "../components/TopDoctors"
 
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
 
     <Header/>
     <SpecialityMenu />
+    <TopDoctors/>
     </div>
     
   )
