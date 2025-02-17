7be0ff606754fdd10f6e4fca850144a4f7c25a9c
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-17 16:25:55 +0530

    speciality menu added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header"
+import SpecialityMenu from "../components/SpecialityMenu"
 
 
 function Home() {
   return (
+    <div>
+
     <Header/>
+    <SpecialityMenu />
+    </div>
+    
   )
 }
 
