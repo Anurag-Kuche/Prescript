33e57a36c1bafb6f1ccb901da1fd4fa49a53e5a6
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-21 10:12:53 +0530

    appointment page in progress

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,6 +1,24 @@
+import { useParams, useSearchParams } from "react-router-dom"
+import { AppContext } from "../context/AppContext"
+import { useEffect } from "react"
 
 
 function Appointment() {
+  const {docId}=useParams()
+  const{doctors}=useContext(AppContext)
+  const[docInfo,setDocInfo]=useState(null)
+
+  const fetchDocInfo=async()=>{
+    const docInfo=doctors.find(doc=>doc._id=docId)
+    setDocInfo(docInfo)
+  }
+
+  useEffect(() => {
+    
+  fetchDocInfo
+    
+  }, [doctors,docId])
+  
   return (
     <div>Appointment</div>
   )
