68033617438e47b3e86a253718c1634d31cc5630
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-24 14:38:49 +0530

    why choose us begins

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,12 @@ function About() {
       </div>
 
       <div className="my-10 flex flex-col md:flex-row gap-12">
-        <img src={assets.about_image} alt="" />
-        <div>
-          <p>Your health, your time—schedule a doctor’s appointment in just a few clicks!.Connect with trusted doctors and book your next appointment seamlessly</p>
+        <img className="w-full md:max-w-[360px]" src={assets.about_image} alt="" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <p>Taking charge of your health has never been easier with our intuitive appointment booking system. Whether you need a routine check-up or specialized treatment, our app connects you to a network of trusted and experienced doctors across various specialties. Simply browse through available doctors, view their credentials, and book an appointment at your convenience. With just a few clicks, you can choose the time that fits your schedule, eliminating the hassle of long wait times and lengthy calls.</p>
           <p>Browse, choose, and book appointments with the best doctors near you</p>
-          <b>our Vision</b>
-          <p>Your health is a priority—book your doctor’s appointment today, hassle-free!</p>
+          <b className="text-gray-800">Our Vision</b>
+          <p>To revolutionize healthcare access by providing a seamless, reliable, and user-friendly platform that connects patients with trusted medical professionals. We aim to simplify the process of booking appointments, ensuring timely and efficient healthcare for everyone. Our vision is to empower individuals to take control of their health by making quality medical care accessible, convenient, and just a few clicks away. We strive to bridge the gap between patients and healthcare providers, fostering a healthier and more connected community.</p>
         </div>
       </div>
     </div>
