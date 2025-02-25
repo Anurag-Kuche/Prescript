310a15c7481a6c5ea9da6ea361af998c72d16eb2
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-25 13:41:37 +0530

    sign up pg started

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
 
 
 function Login() {
+  const [state,setState]=useState("Sign Up")
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const[name,setName]=useState("")
+
+  const onSubmitHandler=async (event)=>{
+    event.preventDefault()
+  }
   return (
-    <div>Login</div>
+    <form className="min-h-[80vh] flex items-center">
+      <div>
+        <p>{state==='Sign Up'? "Create Account":"Login"}</p>
+        <p>Please {state==='Sign Up'? "sign up":"Login" } to book appointment</p>
+      </div>
+
+    </form>
+      
+
+    
   )
 }
 
